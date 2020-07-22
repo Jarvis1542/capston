f3e2ff10e367940d8f05489dae119e333f02c2a1
Seungwoo <seung9721@naver.com>
2020-07-22 15:40:15 +0900

    회원정보 수정 - 회원구분 Form 추가

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -3,3 +3,22 @@ $('.carousel').carousel({
     interval: 4000,
     pause: false // 활성화시 슬라이드에 마우스가 있더라도 슬라이드쇼 허용
 });
+
+// Raido 버튼 값에 따라 내용 표시(memberEdit)
+$('input[type=radio][name=memberType]').on('click', function() {
+    var chkValue = $('input[type=radio][name=memberType]:checked').val();
+
+    if (chkValue == 'typeNormal') {
+        $('#securityForm').css('display', 'none');
+        $('#adminForm').css('display', 'none');
+        // return false;
+    } else if (chkValue == 'typeSecurity') {
+        $('#securityForm').css('display', 'block');
+        $('#adminForm').css('display', 'none');
+        // return false;
+    } else if (chkValue == 'typeAdmin') {
+        $('#securityForm').css('display', 'none');
+        $('#adminForm').css('display', 'block');
+        // return false;
+    }
+});
