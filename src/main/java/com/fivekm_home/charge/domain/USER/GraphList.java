9235ee29604bb05769eabd8fab2e8145742099a6
Seungwoo <seung9721@naver.com>
2020-07-27 00:51:13 +0900

    쿠오오

--- src/main/java/com/fivekm_home/charge/domain/USER/GraphList.java
+++ src/main/java/com/fivekm_home/charge/domain/USER/GraphList.java
@@ -2,9 +2,9 @@ package com.fivekm_home.charge.domain.USER;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 public class GraphList {
-    private LocalDateTime created_date;
+    private Date create_date;
 }
