64461cc7e8dc0d70522f2c890b5dce376818484b
Jeongyeong <pjy4952@gmail.com>
2020-07-27 20:27:00 +0900

    파일 업로드 예시 성공

--- src/main/java/com/fivekm_home/charge/ChargeApplication.java
+++ src/main/java/com/fivekm_home/charge/ChargeApplication.java
@@ -1,9 +1,15 @@
 package com.fivekm_home.charge;
 
+import com.fivekm_home.charge.config.StorageConfig;
+import com.fivekm_home.charge.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableConfigurationProperties(StorageConfig.class)
 @EnableJpaAuditing
 @SpringBootApplication
 public class ChargeApplication {
@@ -12,4 +18,11 @@ public class ChargeApplication {
         SpringApplication.run(ChargeApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
