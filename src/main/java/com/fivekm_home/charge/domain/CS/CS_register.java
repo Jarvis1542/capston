9eb5c2ffe6da6d384a6fc91df6da207b4c72c25c
gulyguy <gulyguy@naver.com>
2020-08-07 11:04:28 +0900

    충전소 등록임 (주소 불러오기로 바꿔야함)

--- src/main/java/com/fivekm_home/charge/domain/CS/CS_register.java
+++ src/main/java/com/fivekm_home/charge/domain/CS/CS_register.java
@@ -9,9 +9,12 @@ public class CS_register {
     private String operation; //운영기관
     private String chargeSpeed; //충전속도
     private String postcode; //우편번호
+    private String chargeAmount; //충전기 수
     private String roadAddress; //도로주소
     private String detailAddress; //상세주소
     private String extraAddress; //참고항목
+    private String min30Fee; //기본요금
+    private String addMin10Fee; //10분당 추가요금
     private String image1; //사진1
     private String image2; //사진 2
     private String cable; //케이블유무
--- src/main/java/com/fivekm_home/charge/mapper/CSMapper.java
+++ src/main/java/com/fivekm_home/charge/mapper/CSMapper.java
@@ -5,3 +5,4 @@ import com.fivekm_home.charge.domain.CS.CS_register;
 public interface CSMapper {
     void csReg(CS_register cs_register);
 }
+
