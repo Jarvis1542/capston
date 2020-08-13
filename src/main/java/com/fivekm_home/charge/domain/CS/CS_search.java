5af3aec9663c8e4388a57ee24718e0d1e7607360
gulyguy <gulyguy@naver.com>
2020-08-13 14:08:38 +0900

    충전소 예약시 충전기잔여개수

--- src/main/java/com/fivekm_home/charge/domain/CS/CS_search.java
+++ src/main/java/com/fivekm_home/charge/domain/CS/CS_search.java
@@ -7,6 +7,7 @@ import lombok.Data;
 public class CS_search  {
     private String chargeName;
     private String OPERATION;
+    private String chargeAmount;
     private int min30Fee;
     private int addMin10Fee;
     private String roadAddress;
