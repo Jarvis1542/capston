146ff292f4f2bf69f6f81a5366c28f48a7de8695
Seungwoo <seung9721@naver.com>
2020-09-09 22:48:45 +0900

    랭킹 작업중(21%)

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -10,13 +10,13 @@ $(function() {
                 "                <div class=\"col-lg-11\">\n" +
                 "                    <div class=\"single_testmonial d-flex align-items-center\">\n" +
                 "                        <div class=\"thumb\">\n" +
-                "                            <img src="+msg[i].scs_PIC+" alt=\"\">\n" +
+                "                            <img src="+msg[i].scs_PIC+" alt=\"\" style=\"width:240px; height:200px; \">\n" +
             "                                <div class=\"quote_icon\">\n" +
             "                                    <i class=\"Flaticon flaticon-quote\"></i>\n" +
             "                                </div>\n" +
             "                        </div>\n" +
             "                        <div class=\"info\">\n" +
-            "                            <p>BEST 1등 주차장</p>\n" +
+            "                            <p>충전소 인기 "+(i+1)+"위</p>\n" +
             "                            <span>"+msg[i].road_ADDR+"<br>\n" +
             "                                대구광역시 북구 복현동 218(복현로 35)<br>\n" +
             "                                010-xxxx-xxxx</span>\n" +
@@ -25,7 +25,45 @@ $(function() {
             "                </div>\n" +
             "            </div>\n" +
             "        </div>");
+
             rank_arr[i] = msg[i];
+        }
+        // review-active
+        var testmonial = $('.testmonial_active2');
+        if(testmonial.length){
+            testmonial.owlCarousel({
+                loop:true,
+                margin:0,
+                autoplay:true,
+                navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+                nav:true,
+                dots:false,
+                autoplayHoverPause: true,
+                autoplaySpeed: 800,
+                responsive:{
+                    0:{
+                        items:1,
+                        dots:false,
+                        nav:false,
+                    },
+                    767:{
+                        items:1,
+                        dots:false,
+                        nav:false,
+                    },
+                    992:{
+                        items:1,
+                        nav:true
+                    },
+                    1200:{
+                        items:1,
+                        nav:true
+                    },
+                    1500:{
+                        items:1
+                    }
+                }
+            });
         }
         console.log("rank_arr",rank_arr);
         console.log("rank_arr1", rank_arr[0].scs_PIC);
